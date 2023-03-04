fdda1d0db43a9a7d24bf753dc0467a80c065f575
AleksandrMayer <Aleksandr_gliznutsa@ukr.net>
2023-03-04 14:42:16 +0200

    HW16.re

--- src/test/java/net/hliznutsa/hw16/PointTest.java
+++ src/test/java/net/hliznutsa/hw16/PointTest.java
@@ -10,26 +10,32 @@ class PointTest {
     @Test
     void testSetX() {
         Point setxPoint = new Point(5, 7);
+
         setxPoint.setX(4);
+
         Assertions.assertEquals(4, setxPoint.getX());
     }
 
     @Test
     void testSetY() {
         Point setyPoint = new Point(5, 7);
+
         setyPoint.setY(9);
+
         Assertions.assertEquals(9, setyPoint.getY());
     }
 
     @Test
     void testGetX() {
         Point getxPoint = new Point(5, 7);
+
         Assertions.assertEquals(5, getxPoint.getX());
     }
 
     @Test
     void testGetY() {
         Point getyPoint = new Point(1, 2);
+
         Assertions.assertEquals(2, getyPoint.getY());
     }
 
@@ -37,20 +43,25 @@ class PointTest {
     void testPointDistance() {
         Point point1 = new Point(5, 7);
         Point point2 = new Point(9, 2);
+
         double result = pointDistance(point1, point2);
-        Assertions.assertEquals(6.4031242374328485, result);
+
+        Assertions.assertEquals(6.4031242374328485, result, 0.001);
     }
 
     @Test
     void testPointDistanceNull() {
         double result = pointDistance(null, null);
+
         Assertions.assertEquals(0, result);
     }
 
     @Test
     void testPointDistanceOneNull() {
         Point point1 = new Point(5, 7);
+
         double result = pointDistance(point1, null);
+
         Assertions.assertEquals(0, result);
     }
 
@@ -58,7 +69,9 @@ class PointTest {
     void testPointDistanceZero() {
         Point point1 = new Point(0, 0);
         Point point2 = new Point(0, 0);
+
         double result = pointDistance(point1, point2);
+
         Assertions.assertEquals(0, result);
     }
 
@@ -66,8 +79,10 @@ class PointTest {
     void testPointDistanceMinusInt() {
         Point point1 = new Point(-7, -8);
         Point point2 = new Point(-10, -6);
+
         double result = pointDistance(point1, point2);
-        Assertions.assertEquals(3.605551275463989, result);
+
+        Assertions.assertEquals(3.605551275463989, result, 0.001);
     }
 
 
@@ -75,14 +90,18 @@ class PointTest {
     void testPointDistanceOnePoint() {
         Point point1 = new Point(11, 9);
         Point point2 = new Point(7, 6);
+
         double result = point2.pointDistanceOnePoint(point1);
+
         Assertions.assertEquals(5, result);
     }
 
     @Test
     void testPointDistanceOnePointNull() {
         Point point = new Point(7, 6);
+
         double result = point.pointDistanceOnePoint(null);
+
         Assertions.assertEquals(0, result);
     }
 
@@ -144,21 +163,40 @@ class PointTest {
     @Test
     void testHashCode() {
         Point point1 = new Point(8, 15);
-        int result = point1.hashCode();
-        Assertions.assertEquals(263, result);
+        Point point2 = new Point(8, 15);
+        Point point3 = new Point(5, 11);
+
+        int resultConsistency1 = point1.hashCode();
+        int resultConsistency2 = point1.hashCode();
+
+        boolean resultEqualsBoolean = point1.equals(point2);
+        int resultEqualsInt = point2.hashCode();
+
+        boolean resultNotEqualsBoolean = point1.equals(point3);
+        int resultNotEqualsInt = point3.hashCode();
+
+        Assertions.assertEquals(resultConsistency1, resultConsistency2);
+        Assertions.assertTrue(resultEqualsBoolean);
+        Assertions.assertEquals(resultConsistency1, resultEqualsInt);
+        Assertions.assertFalse(resultNotEqualsBoolean);
+        Assertions.assertNotEquals(resultEqualsInt, resultNotEqualsInt);
     }
 
     @Test
     void testToString() {
         Point point1 = new Point(8, 15);
+
         String result = point1.toString();
+
         Assertions.assertEquals("X = 8  Y = 15", result);
     }
 
     @Test
     void testClone() throws CloneNotSupportedException {
         Point point1 = new Point(8, 15);
+
         Point clonePoint1 = point1.clone();
+
         Assertions.assertEquals(point1, clonePoint1);
     }
 }
